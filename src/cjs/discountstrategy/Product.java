ac7c00377ae20b13788c405d21e23e410bf3cdf3
Carson Schultz <Carson Schultz@10.30.20.232>
2016-10-06 14:26:21 -0500

    first commit - after renaming project and repository

---
+++ src/cjs/discountstrategy/Product.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cjs.discountstrategy;
+
+/**
+ *
+ * @author Carson Schultz
+ */
+public class Product {
+    private String prodId;
+    private String prodName;
+    private double unitCost;
+    private DiscountStrategy discount;
+
+    public Product(String prodId, String prodName, double unitCost, DiscountStrategy discount) {
+        this.prodId = prodId;
+        this.prodName = prodName;
+        this.unitCost = unitCost;
+        this.discount = discount;
+    }
+    
+    
+    
+    public String getProdId() {
+        return prodId;
+    }
+
+    public void setProdId(String prodId) {
+        this.prodId = prodId;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public double getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(double unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public DiscountStrategy getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(DiscountStrategy discount) {
+        this.discount = discount;
+    }
+
+
+}
